b9c8cd128927a209ea668db428a63688894f2f2c
Tosha177777 <73199654+Tosha177777@users.noreply.github.com>
2024-02-16 22:15:29 +0200

    modal

--- src/pages/Catalog/CatalogPage.jsx
+++ src/pages/Catalog/CatalogPage.jsx
@@ -4,24 +4,39 @@ import CatalogList from "../../components/CatalogList/CatalogList";
 
 const CatalogPage = () => {
   const [cars, setCars] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const limit = 12;
+  // const [loadMore, setLoadMore] = useState(false);
 
   useEffect(() => {
     const fetchAllCars = async () => {
       try {
-        const data = await fetchCars();
-        setCars(data);
+        const newCars = await fetchCars(currentPage, limit);
+        console.log("currentPage: ", currentPage);
+        console.log("newCars: ", newCars.length);
+
+        setCars((prevCars) =>
+          currentPage === 1 ? newCars : [...prevCars, ...newCars]
+        );
       } catch (error) {
         console.error(error);
       }
     };
     fetchAllCars();
-  }, []);
+  }, [currentPage]);
+
+  const loadMore = cars.length >= currentPage * limit;
+
+  const handleClick = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
     <section className="catalog-section">
       <div className="container">
         <p>CatalogPage</p>
         <CatalogList cars={cars} />
+        {loadMore && <button onClick={() => handleClick()}>Load more</button>}
       </div>
     </section>
   );
--- src/services/api.js
+++ src/services/api.js
@@ -4,9 +4,19 @@ const instance = axios.create({
   baseURL: "https://65ce2c89c715428e8b4020f3.mockapi.io/",
 });
 
-const fetchCars = async () => {
-  const { data } = await instance.get("adverts");
-  return data;
+const fetchCars = async (page, limit) => {
+  try {
+    const response = await instance.get("adverts", {
+      params: {
+        page: page,
+        limit: limit,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching cars:", error);
+    throw error;
+  }
 };
 
 export default fetchCars;
